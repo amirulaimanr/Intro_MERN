c43c2a4555715ada8e70409ccab698c2ea132b4b
amirulaimanr <amirulaimanrohaimi@gmail.com>
2023-03-03 16:28:05 +0800

    modified template.js

--- reactTest/react-backend/template.js
+++ reactTest/react-backend/template.js
@@ -1,15 +1,17 @@
 const express = require("express");
-
 const app = express();
-
+const port = 3000;
 const ejs = require("ejs");
+
+// Array of items to display
+const items = ["apple", "banana", "orange", "pear"];
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", { items: ["Item 1", "Item 2", "Item 3"] });
+  res.render("list", { items });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
