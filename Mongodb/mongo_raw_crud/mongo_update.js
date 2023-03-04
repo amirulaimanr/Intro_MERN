34b84f97ea2999438e9a6d437a41ec5f4b777d73
amirulaimanr <amirulaimanrohaimi@gmail.com>
2023-03-04 11:13:55 +0800

    add crud operation file

---
+++ Mongodb/mongo_raw_crud/mongo_update.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require("mongodb");
+
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+// Connection URL
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "test";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("device");
+
+  // the following code examples can be pasted here...
+  const updateResult = await collection.updateOne(
+    { name: "John Doe" },
+    { $set: { age: 31 } }
+  );
+
+  console.log("Updated document", updateResult);
+
+  return "done.";
+}
+
+main().then(console.log).catch(console.error);
+
+//.finally(() => client.close());
